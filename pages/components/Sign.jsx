124ff9970ddfa440ba536a0b73f45e77049c32f6
Gissell Bustos <gissellbustosb@gmail.com>
2023-03-13 19:16:29 +1100

    add last version of the avatar for each signes

--- pages/components/Sign.jsx
+++ pages/components/Sign.jsx
@@ -3,15 +3,34 @@ import styles from "@/styles/Home.module.css";
 
 export default function Sign({ children }) {
   return (
-    <div className={styles.sing}>
-      <Image
-        src={"/vigro.png"}
-        alt="vigro"
-        width={200}
-        height={200}
-        className={styles.singImg}
-      />
-      <p>{children}</p>
-    </div>
+    <>
+      {[
+        "aries",
+        "taurus",
+        "gemini",
+        "cancer",
+        "leo",
+        "virgo",
+        "libra",
+        "scorpio",
+        "sagittarius",
+        "capricorn",
+        "aquarius",
+        "pisces",
+      ].map((path) => {
+        return (
+          <div key={path} className={styles.sing}>
+            <Image
+              src={`/${path}.png`}
+              alt="sign"
+              width={200}
+              height={200}
+              className={styles.singImg}
+            />
+            <p>{`${path}`}</p>
+          </div>
+        );
+      })}
+    </>
   );
 }
--- pages/components/Signes.jsx
+++ pages/components/Signes.jsx
@@ -4,18 +4,7 @@ import styles from "@/styles/Home.module.css";
 export default function Signes() {
   return (
     <div className={styles.signes}>
-      <Sign>Aries</Sign>
-      <Sign>Tarus</Sign>
-      <Sign>Gemini</Sign>
-      <Sign>Cancer</Sign>
-      <Sign>Leo</Sign>
-      <Sign>Vigro</Sign>
-      <Sign>Libra</Sign>
-      <Sign>Scorpio</Sign>
-      <Sign>Sagittairus</Sign>
-      <Sign>Capricorn</Sign>
-      <Sign>Aquarius</Sign>
-      <Sign>Pisces</Sign>
+      <Sign></Sign>
     </div>
   );
 }
