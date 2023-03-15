62f8c6ed8601aae84938e9fcdc5c0aab792a7f42
Gissell Bustos <gissellbustosb@gmail.com>
2023-03-15 16:07:33 +1100

    add links to home signes

--- pages/aries.js
+++ pages/aries.js
@@ -1,9 +1,17 @@
-import Sign from "./components/Sign";
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
+
 export default function Aries() {
   return (
     <>
       <>
-        <Sign>Aries</Sign>
+        <Image
+          src={"/aries.png"}
+          alt="sign"
+          width={200}
+          height={200}
+          className={styles.singImg}
+        ></Image>
         <p> 25 December - January 1</p>
       </>
     </>
--- pages/components/Sign.jsx
+++ pages/components/Sign.jsx
@@ -1,36 +1,41 @@
 import Image from "next/image";
 import styles from "@/styles/Home.module.css";
+import Link from "next/link";
 
 export default function Sign({ children }) {
+  const signes = [
+    "aries",
+    "taurus",
+    "gemini",
+    "cancer",
+    "leo",
+    "virgo",
+    "libra",
+    "scorpio",
+    "sagittarius",
+    "capricorn",
+    "aquarius",
+    "pisces",
+  ];
+
   return (
-    <>
-      {[
-        "aries",
-        "taurus",
-        "gemini",
-        "cancer",
-        "leo",
-        "virgo",
-        "libra",
-        "scorpio",
-        "sagittarius",
-        "capricorn",
-        "aquarius",
-        "pisces",
-      ].map((path) => {
+    <div className={styles.signes}>
+      {signes.map((path) => {
         return (
           <div key={path} className={styles.sing}>
-            <Image
-              src={`/${path}.png`}
-              alt="sign"
-              width={200}
-              height={200}
-              className={styles.singImg}
-            />
-            <p>{`${path}`}</p>
+            <Link href={`${path}`}>
+              <Image
+                src={`/${path}.png`}
+                alt="sign"
+                width={200}
+                height={200}
+                className={styles.singImg}
+              />
+            </Link>
+            <p>{`${path[0].toUpperCase() + path.slice(1, path.length)}`}</p>
           </div>
         );
       })}
-    </>
+    </div>
   );
 }
