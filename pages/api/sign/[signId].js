69fdf3e64815967fc7e47d6691fddda45991ef01
Gissell10 <gissellbustosb@gmail.com.com>
2023-04-18 16:48:02 +1000

    second try to fecth openAI data

--- pages/api/sign/[signId].js
+++ pages/api/sign/[signId].js
@@ -1,28 +1,35 @@
 import { signs } from "../../../constants/signs";
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const { query } = req;
   const { signId } = query;
   const sign = signs.find((s) => s.id === signId);
-  const openAPIKEY = "xldfRRFfijasdfhbosr";
 
   if (sign) {
-    const apiUrl = "https://api.openai.com/v1/engines/text-davinci-002/completions";
+    console.log(process.env.OPENAI_API_KEY, "KEY");
+    const apiUrl =
+      "https://api.openai.com/v1/engines/text-davinci-002/completions";
     const response = await fetch(apiUrl, {
-      method: 'POST',
-      headers:{
+      method: "POST",
+      headers: {
         "Content-Type": "application/json",
-        "Autorization": `Bearer ${process.env.openAPIKEY}`,
-      }, 
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
       body: JSON.stringify({
-        prompt:`Write a daily horoscope for ${sign.id}`, 
-        max_tokens: 300, 
+        prompt: `Write a daily horoscope for ${signId}`,
+        max_tokens: 100,
         temperature: 0.7,
       }),
     });
+    console.log(response);
     const data = await response.json();
-    const horoscope = data.choices[0].text;
-    res.status(200).json({...sign, horoscope});
+
+    if (data && data.choices && data.choices.length > 0) {
+      const horoscope = data.choices[0].text;
+      res.status(200).json({ ...sign, horoscope });
+    } else {
+      res.status(500).json({ message: `Failed to fecth horoscope data` });
+    }
   } else {
     return res
       .status(404)
